5a744e46a7bb9560bb5ad08df7104ee50b334aa9
iamprince8 <vision.iamprince@gmail.com>
2026-09-25 21:21:55 +0530

    Fix socket URL reference

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
 
-export const socket = io(URL, {
+export const socket = io(SERVER_URL, {
   autoConnect: false // We will connect manually when entering multiplayer
 });
